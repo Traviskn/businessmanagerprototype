9d8ae7864f29434378d90da49f6ba41114f68c44
Oscar Villordo <villordooscar@gmail.com>
2015-11-18 10:38:27 -0700

    change product and inventory pages

--- js/edit-product.js
+++ js/edit-product.js
@@ -1,13 +1,15 @@
 jQuery(function($) {
-  var $optionsContainer = $('#costContainer');
-  $optionsContainer.hide();
+  var $quantityContainer = $('#quantityContainer');
+  var $costContainer = $('#costContainer');
+  $quantityContainer.show();
+  $costContainer.show();
 });
 
 jQuery(function($) {
   var $checks = $('.checkbox-container input:checkbox').click(function() {
     var checked = $checks.is(':checked');
-    $('#costContainer').toggle(!checked);
-    $('#linkInv').toggle(checked);
+    $('#quantityContainer').toggle(checked);
+    $('#costContainer').toggle(checked);
   });
 });
 
