81f8d8f422d75caf77d78f9724c568776df0c31b
Travis Nuttall <tuttall@gmail.com>
2015-11-18 22:12:51 -0700

    Rework Sales Page Layout

--- js/sales.js
+++ js/sales.js
@@ -1,112 +1,135 @@
-var ProductList = function({products, addProduct}) {
-  var productList = products.map(function(product) {
-    return (
-      <tr>
-        <td>{product.name}</td>
-        <td>${product.price}</td>
-        <td>
-          <button className="btn-floating waves-effect waves-light btn" onClick={addProduct.bind(undefined, product)}>
-            <i className="material-icons">add</i>
-          </button>
-        </td>
-      </tr>
-    );
-  });
-
+var ProductButton = function({index, product, sellProduct}) {
   return (
-    <table className="bordered striped">
-      <thead>
-        <tr>
-            <th data-field="name">Item</th>
-            <th data-field="price">Price</th>
-            <th>Add</th>
-        </tr>
-      </thead>
+    <div className='center-align row'>
+      <div
+        className='center-align waves-effect waves-light btn-large'
+        style={{
+          height: '120px',
+          width: '100%'
+        }}
+        onClick={() => sellProduct(index)}>
+        <div
+          className='chip left'
+          style={{
+            marginTop: '28px',
+            height: '64px',
+            fontSize: '25px',
+            lineHeight: '64px',
+            padding: '0 24px',
+            borderRadius: '32px'
+          }}>
+          {product.count}
+        </div>
+        <div
+          className='right'
+          style={{
+            marginTop: '36px',
+            marginRight: '50px',
+            fontSize: '25px'
+          }}>
+          $ {product.price}
+        </div>
+        <div
+          style={{
+            marginTop: '36px',
+            fontSize: '25px'
+          }}>
+          {product.name}
+        </div>
+      </div>
+    </div>
+  )
+};
 
-      <tbody>
-        {productList}
-      </tbody>
-    </table>
+var ProductGrid = function({products, sellProduct}) {
+  return (
+    <div style={{marginTop: '1em'}}>
+      {products.map(
+        (product, index) => <ProductButton key={index} product={product} index={index} sellProduct={sellProduct}/>
+      )}
+    </div>
   );
 };
 
-var ShoppingCart = function({cartItems, removeProduct}) {
-
-  var total = 0.00;
-  cartItems.forEach(function(item) {
-    total += item.price;
-  });
-
-  var cartList = cartItems.map(function(item, index) {
-    return (
-      <tr>
-        <td>{item.name}</td>
-        <td>${item.price}</td>
-        <td>
-          <button className="btn-floating btn waves-effect waves-light red" onClick={removeProduct.bind(undefined, index)}>
-            <i className="material-icons">delete</i>
-          </button>
-        </td>
-      </tr>
-    );
-  });
-
+var SalesTotal = function({products}) {
+  var total = 0;
+  for (let i=0; i < products.length; i++) {
+    let subtotal = products[i].price * products[i].count;
+    total += subtotal;
+  }
   return (
-    <div className="card-panel center-align">
-      <h3>Total: ${total}</h3>
-      {cartItems.length > 0 && (
-        <table className="bordered striped">
-          <thead>
-            <tr>
-                <th data-field="name">Item</th>
-                <th data-field="price">Price</th>
-                <th>Remove</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {cartList}
-          </tbody>
-        </table>
-      )}
-      <br />
-      <button className="waves-effect waves-light btn">Checkout</button>
+    <div
+      style={{
+        display: 'inline-block',
+        fontSize: '25px',
+        fontWeight: 'bold',
+        marginRight: '40px'
+      }}>
+      $ {total}
     </div>
   );
+};
+
+var SellButton = function({recordSale}) {
+  return (
+    <div
+      className='center-align waves-effect waves-light btn-large blue'
+      style={{
+        fontSize: '25px'
+      }}
+      onClick={recordSale}>
+      Record Sale
+    </div>
+  );
+};
+
+var SaleSummary = function({products, recordSale}) {
+  return (
+    <div
+      className='card center-align'
+      style={{
+        padding: '25px'
+      }}>
+      <SalesTotal products={products}/>
+      <SellButton recordSale={recordSale}/>
+    </div>
+  )
 };
 
 var SalesPage = React.createClass({
   getInitialState: function() {
     return {
       products: [
-        {name: 'Ring', price: 5.00},
-        {name: 'Scarf', price: 3.25},
-        {name: 'Shirt', price: 2.50}
-      ],
-      cartItems: []
-    }
-  },
-
-  addProductToCart: function(product) {
-    var {cartItems} = this.state;
-    cartItems.push(product);
-    this.setState({cartItems: cartItems});
-  },
-
-  removeProductFromCart: function(index) {
-    var {cartItems} = this.state;
-    cartItems.splice(index, 1);
-    this.setState({cartItems});
+        {name: 'Ring', price: 5.00, count: 0},
+        {name: 'Scarf', price: 3.25, count: 0},
+        {name: 'Shirt', price: 2.50, count: 0}
+      ]
+    };
   },
 
   render: function() {
-    var {cartItems, products} = this.state;
     return (
       <div>
-        <ShoppingCart cartItems={cartItems} removeProduct={this.removeProductFromCart} />
-        <ProductList products={products} addProduct={this.addProductToCart}/>
+        <SaleSummary products={this.state.products} recordSale={this.recordSale}/>
+        <ProductGrid products={this.state.products} sellProduct={this.sellProduct}/>
       </div>
     );
+  },
+
+  sellProduct: function(index) {
+    var products = this.state.products.slice();
+    products[index].count += 1;
+    this.setState({products: products});
+  },
+
+  recordSale: function() {
+    this.setState({
+      products: [
+        {name: 'Ring', price: 5.00, count: 0},
+        {name: 'Scarf', price: 3.25, count: 0},
+        {name: 'Shirt', price: 2.50, count: 0}
+      ]
+    });
   }
 });
 
